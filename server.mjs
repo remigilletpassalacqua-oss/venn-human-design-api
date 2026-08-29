0d28589c4ac3e65c6d158a316750f2b8b7526909
remigilletpassalacqua-oss <remi.gillet.passalacqua@gmail.com>
2026-08-29 19:08:05 +0200

    Update server.mjs
    Serve hexagram images

--- server.mjs
+++ server.mjs
@@ -7,6 +7,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/hexagrams', express.static('public/hexagrams'));
+
 function parseBirthDate(value) {
   if (!value) {
     throw new Error("birthDate is required");
