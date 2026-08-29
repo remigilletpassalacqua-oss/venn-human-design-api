e8303328cd6ec350aeb1e9efa56c4024bb1e8b3a
remigilletpassalacqua-oss <remi.gillet.passalacqua@gmail.com>
2026-08-29 19:51:34 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use('/hexagrams', express.static('public/hexagrams'));
 
 function parseBirthDate(value) {
